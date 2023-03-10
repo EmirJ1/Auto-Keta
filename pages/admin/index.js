791db4a12c478fe5108d390fb72f534eab182622
dutiz <zaimidaut@gmail.com>
2023-03-10 23:10:14 +0100

    login added

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -24,7 +24,17 @@ export default function index({ cars }) {
   )
 }
 
-export async function getServerSideProps() {
+export async function getServerSideProps(ctx) {
+  const myCookie = ctx.req?.cookies || ''
+
+  if (myCookie.token !== process.env.TOKEN) {
+    return {
+      redirect: {
+        destination: '/admin/login',
+        permanent: false,
+      },
+    }
+  }
   const carsProd = await axios.get('https://auto.keta.mk/api/cars')
   return {
     props: {
---
+++ pages/admin/login.js
@@ -0,0 +1,57 @@
+import axios from 'axios'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import SVG from 'react-inlinesvg'
+
+import Button from '@/components/Button'
+import Layout from '@/components/layout/Layout'
+
+export default function Login() {
+  const [username, setUsername] = useState(null)
+  const [password, setPassword] = useState(null)
+  const [error, setError] = useState(false)
+  const router = useRouter()
+
+  async function handleClick() {
+    try {
+      await axios.post('https://auto.keta.mk/api/login', {
+        username,
+        password,
+      })
+      router.push('/admin/')
+    } catch (err) {
+      setError(true)
+    }
+  }
+  return (
+    <Layout title="Login">
+      <div className="container">
+        <div className="row md:my-20 justify-center">
+          <div className="lg:col-6 bg-gray-400 text-center rounded-xl py-5">
+            <SVG src="/svg/person.svg" className="w-40 h-40 mx-auto" />
+            <input
+              type="text"
+              placeholder="Username"
+              className="mt-5 rounded-xl px-8 py-6 w-4/5 placeholder:text-gray-500"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="rounded-xl px-8 py-6 mt-5 w-4/5 placeholder:text-gray-500"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button className="w-3/4 mt-5" onClick={() => handleClick()}>
+              Log In
+            </Button>
+            {error ? (
+              <p className="mt-5">Username or password is incorrect</p>
+            ) : (
+              <p className="mt-5"></p>
+            )}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
