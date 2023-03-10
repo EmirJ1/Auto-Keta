8736ca830a8dde988d37b5e5f0784f7f6fd62b9b
dutiz <zaimidaut@gmail.com>
2023-03-10 22:21:04 +0100

    Admin update

--- components/sections/HeaderSection.js
+++ components/sections/HeaderSection.js
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 
 export default function HeaderSection() {
-  const [mark, setMark] = useState('mark')
+  // const [mark, setMark] = useState('mark')
 
   return (
     <div className="container">
@@ -9,29 +9,17 @@ export default function HeaderSection() {
         <div className="col-12 my-5">
           <div className="flex flex-row">
             <div className="flex flex-col">
-              <select name="mark" onChange={(e) => setMark(e.target.value)}>
-                <option value="audi" onChange={(e) => setMark(e.target.value)}>
-                  Audi
-                </option>
-                <option value="bmw" onChange={(e) => setMark(e.target.value)}>
-                  BMW
-                </option>
-                <option value="mercedes" onChange={(e) => setMark(e.target.value)}>
-                  Mercedes
-                </option>
+              <select name="mark">
+                <option value="audi">Audi</option>
+                <option value="bmw">BMW</option>
+                <option value="mercedes">Mercedes</option>
               </select>
             </div>
             <div className="flex flex-col">
-              <select name="model" onChange={(e) => setModel(e.target.value)}>
-                <option value="audi" onChange={(e) => setModel(e.target.value)}>
-                  Audi
-                </option>
-                <option value="bmw" onChange={(e) => setModel(e.target.value)}>
-                  BMW
-                </option>
-                <option value="mercedes" onChange={(e) => setMark(e.target.value)}>
-                  Mercedes
-                </option>
+              <select name="model">
+                <option value="audi">Audi</option>
+                <option value="bmw">BMW</option>
+                <option value="mercedes">Mercedes</option>
               </select>
             </div>
           </div>
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,70 +1,22 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import axios from 'axios'
-import Router from 'next/router'
 import { useState } from 'react'
-import usePagination from 'utils/usePagination'
 
 import Add from '@/components/Add'
 import Layout from '@/components/layout/Layout'
+import Pagination from '@/components/Pagination'
 export default function index({ cars }) {
-  const _DATA = usePagination(cars, 10)
-  const arr = Array.from({ length: _DATA.maxPage }, (_, i) => i + 1)
   const [add, setAdd] = useState(true)
-  const [carsList, setCarsList] = useState(cars)
-
-  async function handleDelete(id) {
-    try {
-      await axios.delete('http://auto.keta.mk/api/cars/' + id)
-      setCarsList(carsList.filter((car) => car._id !== id))
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.log(err)
-    }
-    Router.reload()
-  }
   return (
     <Layout title="Admin">
-      {console.log(cars)}
+      {/* {console.log(cars)} */}
       <div className="container">
         <div className="row">
           <div className="col-12">
             <button onClick={() => setAdd(false)}>Add</button>
 
             {!add && <Add setAdd={setAdd} />}
-            <table className="mt-5 w-full bg-white rounded-lg">
-              <thead className="px-4 py-2">
-                <tr>
-                  <td className="px-3">Product Id</td>
-                  <td className="px-3">Mark</td>
-                  <td className="text-center">Model</td>
-                  <td>Year</td>
-                  <td>KM</td>
-                  <td>Fuel Type</td>
-                  <td>Transmission</td>
-                  <td>Price</td>
-                  <td>Action</td>
-                </tr>
-              </thead>
-              <tbody>
-                {_DATA.currentData(cars).map((car) => (
-                  <tr className="border-b border-black" key={car._id}>
-                    <td>{car._id}</td>
-                    <td>{car.mark}</td>
-                    <td className="text-center">{car.model}</td>
-                    <td>{car.year}</td>
-                    <td>{car.km}</td>
-                    <td>{car.fueltype}</td>
-                    <td>{car.transmission}</td>
-                    <td>{car.price.toLocaleString()}.00 € </td>
-                    <td>
-                      <button>Edit</button>
-                      <button className="ml-2" onClick={() => handleDelete(car._id)}>
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            <Pagination cars={cars} />
           </div>
         </div>
       </div>
