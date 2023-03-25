07e8f0f2b4a34f67a53e507825412aa07f7c6d7f
EmirJ1 <116313941+EmirJ1@users.noreply.github.com>
2023-03-25 18:45:23 +0100

    Update Button.js

--- components/Button.js
+++ components/Button.js
@@ -2,7 +2,7 @@
 import React from 'react'
 
 const style =
-  'px-6 py-4 bg-gradient bg-gradient-to-r border border-red-500 hover:border-transparent bg-transparent hover:text-white hover:from-white-500 hover:to-gray-black rounded-3xl transition-all ease-in-out duration-500'
+  'px-4 py-2 bg-gradient bg-gradient-to-r border border-gray-700 hover:border-transparent bg-transparent hover:text-black hover:from-white-500 hover:to-gray-black rounded-3xl transition-all ease-in-out duration-500'
 
 export default function Button({ className, children, onClick }) {
   return (
