aaf7403474cb346a6ea19656fa5a1e23484ab225
dutiz <zaimidaut@gmail.com>
2023-03-29 23:48:32 +0200

    header section update

--- components/sections/HeaderSection.js
+++ components/sections/HeaderSection.js
@@ -1,25 +1,21 @@
-// import { useState } from 'react'
-import ReactImageGallery from 'react-image-gallery'
 export default function HeaderSection() {
-  // const [mark, setMark] = useState('mark')
-
-  const images = [
-    {
-      original:
-        'https://res.cloudinary.com/drujet4ue/image/upload/v1679360394/uploads/jjhre8p4r7ut1wn4qam2.jpg',
-    },
-    {
-      original:
-        'https://res.cloudinary.com/drujet4ue/image/upload/v1679360394/uploads/jjhre8p4r7ut1wn4qam2.jpg',
-    },
-    {
-      original:
-        'https://res.cloudinary.com/drujet4ue/image/upload/v1679360394/uploads/jjhre8p4r7ut1wn4qam2.jpg',
-    },
-  ]
   return (
-    <div className="row">
-      <ReactImageGallery items={images} />
+    <div
+      className="relative top-0 flex items-center w-full h-full"
+      style={{
+        background: `linear-gradient(0deg, rgba(17, 17, 17, 0.75), rgba(17, 17, 17, 0.75)), url('./images/header-bg.jpg') no-repeat center center / cover`,
+        height: '700px',
+      }}
+    >
+      <div className="container">
+        <div className="row items-center justify-center">
+          <div className="lg:col-8">
+            <h1 className="lg:text-5xl text-3xl font-bold text-white text-center">
+              Vetura te importuara nga Zvicra
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
