9f94e066097fa530db6b9aafaf241f624e59c129
Daut Zaimi <34359003+dutiz@users.noreply.github.com>
2023-03-30 22:12:11 +0200

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -3,7 +3,5 @@ import React from 'react'
 import Layout2 from '@/components/layout/Layout2'
 
 export default function contact() {
-  return <Layout2 title="Contact">
-    
-  </Layout2>
+  return <Layout2 title="Contact"></Layout2>
 }
