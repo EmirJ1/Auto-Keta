789a7ca36c6a1cb2523027f355dd3b362645ca7e
EmirJ1 <emir.jakupi21@gmail.com>
2023-04-05 04:56:58 +0200

    some updates

--- components/CarsItem.js
+++ components/CarsItem.js
@@ -6,7 +6,7 @@ import SVG from 'react-inlinesvg'
 
 export default function CarsItem({ cars }) {
   return (
-    <div className="lg:col-4 md:col-6 mb-8 ">
+    <div className="lg:col-4 md:col-6 mb-8">
       <Link href={`/cars/${cars._id}`} legacyBehavior>
         <a>
           <div className="bg-white p-1 rounded-xl">
@@ -30,24 +30,15 @@ export default function CarsItem({ cars }) {
               className="w-full rounded-xl cars-img"
             />
 
-            <div className="row justify-center p-4 ">
+            <div className="row justify-center p-4 hover:text-red-600">
               <div className="col-12 ">
                 <p className="text-xl font-semibold my-2 text-center">{`${cars.mark} ${cars.model} ${cars.motor}`}</p>
               </div>
               <div className="col-6">
-                <div className="w-full inline-flex items-center">
-                  <SVG src="/svg/calendar.svg" className="w-7 h-7" />
-                  <p className="ml-2">{cars.year}</p>
-                </div>
+                <div className="w-full inline-flex items-center"></div>
               </div>
               <div className="col-6">
-                <div className="w-full inline-flex justify-end">
-                  <SVG src="/svg/carkm.svg" className="w-8 h-8" />
-                  <p className="ml-2">{cars.km}</p>
-                </div>
-              </div>
-              <div className="col-6 mt-5 text-center">
-                <p className="font-bold text-xl">{cars.price.toLocaleString('en-US')} €</p>
+                <div className="w-full inline-flex justify-end"></div>
               </div>
             </div>
           </div>
